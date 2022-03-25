31fab763ee925c5a97503181fcad1fe4e9de43b3
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-25 13:52:54 +0530

    Made it check compliant

--- src/main/java/com/example/springbootdocker/controllers/TransactionController.java
+++ src/main/java/com/example/springbootdocker/controllers/TransactionController.java
@@ -3,35 +3,46 @@ package com.example.springbootdocker.controllers;
 import com.example.springbootdocker.exceptions.InvalidTransactionReferenceException;
 import com.example.springbootdocker.model.Transaction;
 import com.example.springbootdocker.service.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class for Transactions.
+ */
 @RestController
 public class TransactionController {
 
-    @Autowired
-    TransactionService transactionService;
+  @Autowired
+  TransactionService transactionService;
 
-    @PostMapping("/create")
-    public Long createTransaction(@RequestBody Transaction transaction) {
-        transactionService.saveTransaction(transaction);
-        return transaction.getTransactionId();
+  @PostMapping("/create")
+  public Long createTransaction(@RequestBody Transaction transaction) {
+    transactionService.saveTransaction(transaction);
+    return transaction.getTransactionId();
+  }
+
+  @GetMapping("/viewall")
+  public Iterable<Transaction> viewAllTransactions() {
+    return transactionService.getTransactionHistory();
+  }
+
+  /**
+   * {@summary <p>This is Summary.</p>}
+   *
+   * @param id - transaction id
+   */
+  @GetMapping("/view/{id}")
+  public Transaction viewTransactionById(@PathVariable("id") Long id) {
+    Optional<Transaction> transaction = transactionService.getTransaction(id);
+    if (transaction.isPresent()) {
+      return transaction.get();
     }
 
-    @GetMapping("/viewall")
-    public Iterable<Transaction> viewAllTransactions() {
-        return transactionService.getTransactionHistory();
-    }
-
-    @GetMapping("/view/{id}")
-    public Transaction viewTransactionById(@PathVariable("id") Long id) {
-        Optional<Transaction> transaction = transactionService.getTransaction(id);
-        if(transaction.isPresent()) {
-            return transaction.get();
-        }
-
-        throw new InvalidTransactionReferenceException("Invalid transaction reference provided");
-    }
+    throw new InvalidTransactionReferenceException("Invalid transaction reference provided");
+  }
 }
--- src/main/java/com/example/springbootdocker/exceptions/InvalidTransactionReferenceException.java
+++ src/main/java/com/example/springbootdocker/exceptions/InvalidTransactionReferenceException.java
@@ -1,11 +1,14 @@
 package com.example.springbootdocker.exceptions;
 
+/**
+ * Exception Class for Invalid Transaction reference.
+ */
 public class InvalidTransactionReferenceException extends RuntimeException {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    public InvalidTransactionReferenceException(String errorMessage) {
-        super(errorMessage);
-    }
+  public InvalidTransactionReferenceException(String errorMessage) {
+    super(errorMessage);
+  }
 
 }
